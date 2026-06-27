cd59370306be1a49f2f511e0c812bfe9aba72be4
vinitsharma6433-cpu <vinitsharma6433@gmail.com>
2026-06-27 09:31:13 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'khata-bhai-v1';
+const FILES = ['./KhataBhai.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
